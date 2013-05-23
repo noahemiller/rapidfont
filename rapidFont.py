ab162539402b13baac7891e23a018d98d7be357e
kryp71c <chenkel00@gmail.com>
2013-05-23 12:46:48 -0400

    deleted unused glyphs in ufo, added a, c, space to be generated, added code to construct x-height and descender straights, added background colors to base component glyphs

--- rapidFont.py
+++ rapidFont.py
@@ -1,7 +1,18 @@
 from robofab.world import CurrentGlyph,CurrentFont
 from robofab.interface.all.dialogs import AskString, Message
+import sys
 
+font = CurrentFont()
 
+#testing to make sure the required components are present
+meta = ['round','roundleg','straight']
+for glyph in meta:
+    if glyph not in font:
+        #we exit here ... possibly expand to create blank component glyphs and promt user to create contours.
+        sys.exit('Required component not found. Please use the fontBuilder.ufo supplied with this script.')
+    elif len(font[glyph]) == 0:
+        sys.exit('Required component ' + glyph + ' has no content. Please add contours to ' + glyph)
+        
 
 offset = AskString("Offset components by how many units??")
 if offset == None:
@@ -12,36 +23,96 @@ if offset or offset == 0:
     except ValueError:
         Message("Numbers only.")
     else:
-        font = CurrentFont()
         weight = int(font["straight"].box[2])
         roundWidth=int(font["round"].box[2])
         descender = font.info.descender
         xheight = font.info.xHeight
         ascender = font.info.ascender
-        buildG = ['b','d','p','q','o','h','i','l','n','m','u','test']
+        buildG = ['a','b','c','d','h','i','l','m','n','o','p','q','u','space','straightX','straightD']
 
         for glyph in buildG:
             font.newGlyph(glyph)
             
-       
+        #building an x-height straight by moving the two highest points of the straight down
+        sX = font['straightX']
+        sX.appendGlyph(font['straight'])
+        pts = [0,0]
+        for contour in sX:
+            for point in contour.points:
+                if point == contour.points[0]:
+                    pts[0] = point
+                    pts[1] = point
+                if point.y > pts[0].y:
+                    pts[1] = pts[0]
+                    pts[0] = point
+                elif point.y >= pts[1].y:
+                    pts[1] = point
+        pts[0].y -= (ascender - xheight)
+        pts[1].y -= (ascender - xheight)
+        for contour in sX:
+            contour.update()
+        sX.rightMargin=font['straight'].rightMargin
+        sX.leftMargin=font['straight'].leftMargin
+        sX.mark = (1,.7,0,1)
+        
+        #building a descender straight by moving the two lowest points of the straight up
+        sD = font['straightD']
+        sD.appendGlyph(font['straight'])
+        sD.move((0,-(ascender-xheight)))
+        pts = [0,0]
+        for contour in sD:
+            for point in contour.points:
+                if point == contour.points[0]:
+                    pts[0] = point
+                    pts[1] = point
+                if point.y < pts[0].y:
+                    pts[1] = pts[0]
+                    pts[0] = point
+                elif point.y <= pts[1].y:
+                    pts[1] = point
+        if pts[0].y < descender:
+            pts[1].y += abs(pts[0].y - descender)
+            pts[0].y += abs(pts[0].y - descender)
+        for contour in sD:
+            contour.update()
+        sD.rightMargin=font['straight'].rightMargin
+        sD.leftMargin=font['straight'].leftMargin
+        sD.mark = (1,.7,0,1)
+
+        space = font['space']
+        space.width = roundWidth
+
+        a = font['a']
+        a.appendGlyph(font['round'])
+        a.scale((-1,1))
+        a.move((roundWidth,0))
+        a.appendComponent('straightX',(roundWidth+offset-font['round'].leftMargin-weight/2,0))
+        a.leftMargin=font['round'].rightMargin
+        a.rightMargin=font['straight'].rightMargin
         
         b = font['b']
         b.appendComponent('straight')
-        b.appendComponent('round',(weight+offset-font['round'].leftMargin,0))
+        b.appendComponent('round',(weight/2+offset-font['round'].leftMargin,0))
         b.rightMargin=font['round'].rightMargin
         b.leftMargin=font['straight'].rightMargin
+        
+        c = font['c']
+        c.appendGlyph(font['round'])
+        c.scale((-1,1))
+        c.leftMargin=font['round'].rightMargin
+        c.rightMargin=font['round'].rightMargin - 10
 
         d = font['d']
         d.appendGlyph(font['round'])
         d.scale((-1,1))
         d.move((roundWidth,0))
-        d.appendComponent('straight',(roundWidth+offset-font['round'].leftMargin,0))
+        d.appendComponent('straight',(roundWidth+offset-font['round'].leftMargin-weight/2,0))
         d.rightMargin=font['straight'].rightMargin
         d.leftMargin=font['round'].rightMargin
 
         p = font['p']
-        p.appendComponent('straight',(0,descender))
-        p.appendComponent('round',(weight+offset-font['round'].leftMargin,0))
+        p.appendComponent('straightD')
+        p.appendComponent('round',(weight/2+offset-font['round'].leftMargin,0))
         p.rightMargin=font['round'].rightMargin
         p.leftMargin=font['straight'].rightMargin
 
@@ -49,7 +120,7 @@ if offset or offset == 0:
         q.appendGlyph(font['round'])
         q.scale((-1,1))
         q.move((roundWidth,0))
-        q.appendComponent('straight',(roundWidth+offset-font['round'].leftMargin,descender))
+        q.appendComponent('straightD',(roundWidth+offset-font['round'].leftMargin-weight/2,0))
         q.rightMargin=font['straight'].rightMargin
         q.leftMargin=font['round'].rightMargin
 
@@ -68,8 +139,7 @@ if offset or offset == 0:
         h.leftMargin=font['straight'].rightMargin
         
         i = font['i']
-        i.appendGlyph(font['straight'])
-        i.move((0,(xheight-ascender)))
+        i.appendGlyph(font['straightX'])
         i.rightMargin=font['straight'].rightMargin
         i.leftMargin=font['straight'].rightMargin
         
@@ -80,15 +150,13 @@ if offset or offset == 0:
         
         
         n = font['n']
-        n.appendGlyph(font['straight'])
-        n.move((0,(xheight-ascender)))
+        n.appendGlyph(font['straightX'])
         n.appendComponent('roundleg',(weight+offset,0))
         n.rightMargin=font['roundleg'].rightMargin
         n.leftMargin=font['straight'].rightMargin
         
         m = font['m']
-        m.appendGlyph(font['straight'])
-        m.move((0,(xheight-ascender)))
+        m.appendGlyph(font['straightX'])
         m.appendComponent('roundleg',(weight+offset*3,0))
         m.appendComponent('roundleg',(weight+font['roundleg'].box[2]+offset*10,0))
         m.rightMargin=font['roundleg'].rightMargin
@@ -104,7 +172,10 @@ if offset or offset == 0:
         u = font['u']
         u.appendGlyph(font['n'])
         u.scale((-1,-1))
-        u.move((0,500))
+        u.move((0,xheight))
         u.rightMargin = n.leftMargin
         u.leftMargin = n.rightMargin
-
+        
+        mySorted = buildG + meta
+        font.glyphOrder = mySorted
+        print 'Font created.'
